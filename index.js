68d000708d2fc7362f13fc23b29d4a64fa4bd65a
janejiunkim <janejiunkim@gmail.com>
2021-08-27 16:41:50 -0700

    finished the tiny bit

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ app.post('/dinosaurs', function(req, res) {
     console.log(req.body);
   });
 
+  app.post('/dinosaurs', function(req, res) {
+    // read dinosaurs file
+    const dinosaurs = fs.readFileSync('./dinosaurs.json');
+    dinosaurs = JSON.parse(dinosaurs);
+  
+    // add item to dinosaurs array
+    dinosaurs.push(req.body);
+  
+    // save dinosaurs to the data.json file
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinosaurs));
+  
+    //redirect to the GET /dinosaurs route (index)
+    res.redirect('/dinosaurs');
+  });
+
 app.get('/', function(req, res) {
     res.render('home');
   });
@@ -48,6 +63,21 @@ app.get('/dinosaurs/:idx', function(req, res) {
   
     //render page with data of the specified animal
     res.render('dinosaurs/show', {myDino: dinoData[dinoIndex]});
+  });
+  
+  app.get('/dinosaurs', function(req, res) {
+    const dinosaurs = fs.readFileSync('./dinosaurs.json');
+    const dinoData = JSON.parse(dinosaurs);
+  
+    const nameFilter = req.query.nameFilter;
+  
+    if (nameFilter) {
+      dinoData = dinoData.filter(function(dino) {
+        return dino.name.toLowerCase() === nameFilter.toLowerCase();
+      });
+    }
+  
+    res.render('dinosaurs/index', {myDinos: dinoData});
   });
 
 app.listen(PORT, () => {
